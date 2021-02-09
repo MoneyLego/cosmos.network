48839dd662287a2405b80cd48a321bc9d98bc6c4
Cyrus Goh <hello@lovincyrus.com>
2021-02-09 12:23:05 -0800

    tweak /learn/faq, add _slug

---
+++ pages/learn/_slug.vue
@@ -0,0 +1,41 @@
+<template>
+  <article>
+    <div class="section tm-section-container section-container">
+      <h1>{{ article.title }}</h1>
+      <!-- content from markdown -->
+      <nuxt-content :document="article" />
+      <!-- prevNext component -->
+      <PrevNext :prev="prev" :next="next" class="mt-8" />
+    </div>
+    <section-form />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+    const tagsList = await $content('tags')
+      .only(['name', 'slug'])
+      .where({ name: { $containsAny: article.tags } })
+      .fetch()
+    const tags = Object.assign({}, ...tagsList.map((s) => ({ [s.name]: s })))
+    const [prev, next] = await $content('articles')
+      .only(['title', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .surround(params.slug)
+      .fetch()
+    return {
+      article,
+      tags,
+      prev,
+      next,
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+h1
+  text-align center
+</style>
