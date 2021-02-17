80ce58301c1f2d20b842a385072ababbdcc46154
Cyrus Goh <hello@lovincyrus.com>
2021-02-17 02:15:45 -0800

    move banner and community components to features and staking

--- pages/features.vue
+++ pages/features.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="section-features tm-wrapper">
-    <div class="tm-section tm-container">
-      <div class="title tm-rf6 tm-bold tm-lh-title">
-        The next wave of interchain infrastructure, commerce, and culture.
+  <main>
+    <div class="section-features tm-wrapper">
+      <div class="tm-section tm-container">
+        <div class="title tm-rf6 tm-bold tm-lh-title">
+          The next wave of interchain infrastructure, commerce, and culture.
+        </div>
       </div>
     </div>
-  </div>
+
+    <!-- OTHER SECTION CONTENT GOES HERE -->
+
+    <section-community />
+  </main>
 </template>
 
 <script>
--- pages/staking.vue
+++ pages/staking.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="section-staking tm-wrapper">
-    <div class="tm-section tm-container">
-      <div class="title tm-rf6 tm-bold tm-lh-title">Staking</div>
+  <main>
+    <div class="section-staking tm-wrapper">
+      <div class="tm-section tm-container">
+        <div class="title tm-rf6 tm-bold tm-lh-title">Staking</div>
+      </div>
     </div>
-  </div>
+
+    <!-- OTHER SECTION CONTENT GOES HERE -->
+
+    <section-banner />
+  </main>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus" scop SectionBannered>
 .section-staking
   position relative
   display flex
