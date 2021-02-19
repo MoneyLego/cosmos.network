9b5271aba924b4cb66bd0408754f5f3e5582097c
Cyrus Goh <hello@lovincyrus.com>
2021-02-18 17:44:25 -0800

    rewrite section-banner, add graphics comment

--- components/SectionBanner.vue
+++ components/SectionBanner.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="tm-wrapper tm-section">
     <div class="tm-container tm-grid-base banner-item">
-      <div class="banner-item__title tm-rf5 tm-bold tm-lh-title">
-        Become a Cosmonaut.
+      <div class="text">
+        <div class="title tm-rf5 tm-bold tm-lh-title">Become a Cosmonaut.</div>
+        <div class="subtitle tm-rf1 tm-lh-copy">
+          Learn about staking ATOMs on the Cosmos Hub.
+        </div>
+        <tm-button
+          to-link="internal"
+          to="/staking"
+          size="l"
+          color="var(--white)"
+          background-color="#171717"
+          class="btn"
+          >Stake ATOMs</tm-button
+        >
       </div>
-      <div class="banner-item__subtitle tm-rf1 tm-lh-copy">
-        Learn about staking ATOMs on the Cosmos Hub.
-      </div>
-      <tm-button
-        to-link="internal"
-        to="/staking"
-        size="l"
-        color="var(--white)"
-        background-color="#171717"
-        glow
-        class="banner-item__btn"
-        >Stake ATOMs</tm-button
-      >
+      <!-- GRAPHICs -->
     </div>
   </div>
 </template>
@@ -26,6 +26,14 @@ export default {}
 </script>
 
 <style lang="stylus" scoped>
+.dark-mode .banner-item
+  color var(--white)
+  background #171717
+
+.light-mode .banner-item
+  color var(--black)
+  background var(--white)
+
 .banner-item
   padding var(--spacing-10)
   display block
@@ -33,16 +41,20 @@ export default {}
   hover-raise(-3px)
   box-shadow var(--elevation-4)
 
-  &__subtitle
-    margin-top var(--spacing-5)
+.banner-item .text
+  grid-column span 12
 
-  &__btn
-    margin-top var(--spacing-8)
+.banner-item .title
+  width fit-content
 
-.dark-mode .banner-item
-  color var(--white)
-  background #171717
-.light-mode .banner-item
-  color var(--black)
-  background var(--white)
+.banner-item .subtitle
+  margin-top var(--spacing-5)
+  width fit-content
+
+.banner-item .btn
+  margin-top var(--spacing-8)
+
+@media $breakpoint-medium
+  .banner-item .text
+    grid-column 2/span 9
 </style>
