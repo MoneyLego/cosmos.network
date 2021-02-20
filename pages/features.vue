d89b40780f31db3c64ab03050f684a0b8a6b2f54
Cyrus Goh <hello@lovincyrus.com>
2021-02-19 17:20:53 -0800

    /features - wip features-2, features-7

--- pages/features.vue
+++ pages/features.vue
@@ -41,32 +41,44 @@
       <!-- GRAPHICS -->
     </div>
 
-    <!-- OTHER SECTION CONTENT GOES HERE -->
-    <div class="tm-section tm-wrapper tm-container">
-      <div class="title tm-rf5 tm-bold tm-lh-title">
-        Security and scalability
+    <!-- features-1 GRAPHICS -->
+
+    <!-- features-2 -->
+    <div class="tm-wrapper">
+      <div class="tm-section tm-container tm-grid-base">
+        <div class="features-2-text">
+          <div class="title tm-rf5 tm-bold tm-lh-title">
+            Cutting-edge Features.
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="tm-section tm-wrapper tm-container">
-      <div class="title tm-rf5 tm-bold tm-lh-title">
-        Services generate fees. Fees generate rewards.
+    <!-- features-7 -->
+    <div class="tm-wrapper">
+      <div class="tm-section tm-container tm-grid-base features-7">
+        <!-- GRAPHICS -->
+        <div class="title tm-rf5 tm-bold tm-lh-title">
+          Services generate fees. Fees generate rewards.
+        </div>
+        <div class="description">
+          <div class="subtitle tm-rf1 tm-lh-copy">
+            The more activity on the Hub, the more fees paid by services, which
+            in turn, generates rewards for staked ATOM holders.
+          </div>
+          <tm-button
+            to-link="internal"
+            to="/staking"
+            size="l"
+            dark-color="var(--white)"
+            light-color="var(--black)"
+            background-color="transparent"
+            variant="text"
+            class="btn"
+            >What is staking <span class="icon__right">--></span></tm-button
+          >
+        </div>
       </div>
-      <div class="subtitle tm-rf1 tm-lh-copy">
-        The more activity on the Hub, the more fees paid by services, which in
-        turn, generates rewards for staked ATOM holders.
-      </div>
-      <tm-button
-        to-link="internal"
-        to="/staking"
-        size="l"
-        dark-color="var(--white)"
-        light-color="var(--black)"
-        background-color="transparent"
-        variant="text"
-        class="btn"
-        >What is staking <span class="icon__right">--></span></tm-button
-      >
     </div>
 
     <section-community />
@@ -90,6 +102,23 @@ export default {}
 .features-0-text .split .right .subtitle
   margin-bottom var(--spacing-6)
 
+// FEATURES 1
+// FEATURES 2
+.features-2-text
+  grid-column span 12
+
+// FEATURES 7
+.features-7
+  .title,
+  .description
+    grid-column span 12
+
+.features-7 .description .subtitle
+  margin-top var(--spacing-8)
+
+.features-7 .description .btn
+  margin-top var(--spacing-6)
+
 @media $breakpoint-medium
   // FEATURES 0
   .features-0-text .title,
@@ -106,4 +135,16 @@ export default {}
     grid-column unset
     height 8.5rem
     margin-top 0
+
+  // FEATURES 1
+  // FEATURES 2
+  .features-2-text
+    grid-column 3/span 12
+
+  // FEATURES 7
+  .features-7 .title
+    grid-column 5/span 12
+
+  .features-7 .description
+    grid-column 6/span 10
 </style>
