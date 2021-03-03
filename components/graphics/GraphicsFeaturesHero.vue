047bdfb69dddec38b58b82dcb73a431275b0df98
Cyrus Goh <hello@lovincyrus.com>
2021-03-02 17:07:02 -0800

    /features - add section label, wip graphics-features-hero

---
+++ components/graphics/GraphicsFeaturesHero.vue
@@ -0,0 +1,137 @@
+<template>
+  <svg
+    width="1440"
+    height="2414"
+    viewBox="0 0 1440 2414"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M-450.34 805.132C-712.016 1049.78 -435.228 1580.84 167.882 1991.3C770.993 2401.75 1472.04 2536.17 1733.72 2291.52C1995.39 2046.88 1718.6 1515.81 1115.49 1105.36C512.383 694.903 -188.665 560.487 -450.34 805.132Z"
+      fill="url(#paint0_radial-035273)"
+    />
+    <g filter="url(#filter0_f-209375)">
+      <path
+        d="M366 1401.5C366 1575.19 506.806 1716 680.5 1716C854.194 1716 995 1575.19 995 1401.5C995 1227.81 854.194 1087 680.5 1087C506.806 1087 366 1227.81 366 1401.5Z"
+        fill="url(#paint1_radial-391331)"
+      />
+    </g>
+    <path
+      d="M479.003 1781.43C258.919 1654.36 77.9826 1507.63 -36.6398 1373.12C-93.9517 1305.86 -134.676 1241.68 -155.43 1184.54C-176.185 1127.4 -176.958 1077.35 -154.429 1038.33C-131.899 999.303 -88.1662 974.947 -28.3052 964.352C31.5541 953.757 107.505 956.93 194.406 972.936C368.206 1004.95 585.749 1088.28 805.833 1215.34C1025.92 1342.41 1206.85 1489.14 1321.48 1623.65C1378.79 1690.91 1419.51 1755.09 1440.27 1812.23C1461.02 1869.37 1461.79 1919.42 1439.26 1958.44C1416.74 1997.47 1373 2021.82 1313.14 2032.42C1253.28 2043.01 1177.33 2039.84 1090.43 2023.83C916.63 1991.82 699.087 1908.49 479.003 1781.43Z"
+      stroke="white"
+      stroke-opacity="0.1"
+      stroke-width="0.490261"
+    />
+    <circle cx="476.999" cy="924" r="113" fill="url(#paint2_radial-095474)" />
+    <path
+      d="M1318.01 478.766C1172.38 517.789 1022.68 431.362 983.657 285.725C944.634 140.089 1031.06 -9.60687 1176.7 -48.63C1322.33 -87.6531 1472.03 -1.22609 1511.05 144.41C1550.08 290.046 1463.65 439.742 1318.01 478.766Z"
+      fill="url(#paint3_radial-290366)"
+    />
+    <circle
+      cx="221.999"
+      cy="1569"
+      r="156"
+      transform="rotate(-64.6341 221.999 1569)"
+      fill="url(#paint4_radial-189244)"
+    />
+    <circle
+      cx="1173.5"
+      cy="1297.5"
+      r="102.5"
+      transform="rotate(90 1173.5 1297.5)"
+      fill="url(#paint5_radial-909816)"
+    />
+    <defs>
+      <filter
+        id="filter0_f-209375"
+        x="300.077"
+        y="1021.08"
+        width="760.847"
+        height="760.847"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation="32.9617"
+          result="effect1_foregroundBlur"
+        />
+      </filter>
+      <radialGradient
+        id="paint0_radial-035273"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(670.911 1410.48) rotate(-57.1468) scale(589.717 820.971)"
+      >
+        <stop offset="0.135417" stop-color="#C01AEA" />
+        <stop offset="0.539562" stop-color="#B347CE" stop-opacity="0.3" />
+        <stop offset="1" stop-opacity="0" />
+      </radialGradient>
+      <radialGradient
+        id="paint1_radial-391331"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(680.5 1401.5) rotate(90) scale(302.012)"
+      >
+        <stop offset="0.223958" stop-color="#A5FBFF" />
+        <stop offset="0.385417" stop-color="#86A1FF" stop-opacity="0.7" />
+        <stop offset="0.729167" stop-color="#CF36D2" stop-opacity="0.28" />
+        <stop offset="1" stop-color="#99309B" stop-opacity="0" />
+      </radialGradient>
+      <radialGradient
+        id="paint2_radial-095474"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(429.098 871.968) rotate(47.7044) scale(208.745)"
+      >
+        <stop offset="0.473925" stop-color="#111111" />
+        <stop offset="1" stop-color="#7C2384" />
+      </radialGradient>
+      <radialGradient
+        id="paint3_radial-290366"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1131.6 89.3306) rotate(47.7044) scale(504.438)"
+      >
+        <stop offset="0.473925" />
+        <stop offset="0.866261" stop-color="#211822" />
+      </radialGradient>
+      <radialGradient
+        id="paint4_radial-189244"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(155.871 1497.17) rotate(47.7044) scale(288.179)"
+      >
+        <stop offset="0.473925" stop-color="#111111" />
+        <stop offset="1" stop-color="#7C2384" />
+      </radialGradient>
+      <radialGradient
+        id="paint5_radial-909816"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1130.05 1250.3) rotate(47.7044) scale(189.348)"
+      >
+        <stop offset="0.473925" stop-color="#111111" />
+        <stop offset="1" stop-color="#7C2384" />
+      </radialGradient>
+    </defs>
+  </svg>
+</template>
